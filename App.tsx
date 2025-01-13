922ae2881c990dccac9717c0c48ae8c57c8c39b6
MOUJITX <moujitx_CHN@qq.com>
2025-01-13 12:33:05 +0800

    Fix import formatting in App.tsx and index.js

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import {AppNavigation} from './src/navigation/AppNavigation';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {StatusBar} from 'react-native';
+import { AppNavigation } from './src/navigation/AppNavigation';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
--- index.js
+++ index.js
@@ -2,12 +2,12 @@
  * @format
  */
 import React from 'react';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-import {Provider} from 'react-redux';
-import {persistor, store} from './src/store/store';
-import {PersistGate} from 'redux-persist/integration/react';
+import { name as appName } from './app.json';
+import { Provider } from 'react-redux';
+import { persistor, store } from './src/store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 AppRegistry.registerComponent(appName, () => () => (
   <Provider store={store}>
