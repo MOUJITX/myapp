2e6fc6b447b1c1e91c79e4f21bfa93bc35ca8a11
MOUJITX <moujitx_chn@qq.com>
2025-02-09 17:50:06 +0800

    Add react-native-worklets-core and react-native-vision-camera

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,37 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('metro-config');
+const path = require('path');
 
-const config = getDefaultConfig(__dirname);
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
 
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer'),
-};
-
-config.resolver = {
-  ...config.resolver,
-  assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
-  sourceExts: [...config.resolver.sourceExts, 'svg'],
-};
-
-module.exports = config;
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+      assetRegistryPath: 'react-native/Libraries/Image/AssetRegistry',
+    },
+    resolver: {
+      assetExts: [
+        ...assetExts.filter(ext => ext !== 'svg'),
+        'png',
+        'jpg',
+        'jpeg',
+        'gif',
+      ],
+      sourceExts: [...sourceExts, 'svg'],
+      extraNodeModules: new Proxy(
+        {},
+        {
+          get: (_, name) => path.join(__dirname, `node_modules/${name}`),
+        }
+      ),
+    },
+  };
+})();
