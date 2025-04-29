95ec44fb42f46b46e781032df08358ac4be14ecd
moujitx <moujitx_chn@qq.com>
2025-04-29 20:39:29 +0800

    Add env vars and .env to Android CI for secure config

--- src/environment.ts
+++ src/environment.ts
@@ -6,13 +6,13 @@ export const realBundleID = __DEV__ ? `${bundleID}.dev` : bundleID;
 export const localFileFolder = `file://${RNFS.DocumentDirectoryPath}/`;
 
 export const ossUploadURL = 'https://upload.qiniup.com/';
-export const ossAccessKey = 'ac6g1GmKe43O_7ZBF26jdts61pEN6wUofL3MK603';
-export const ossSecretKey = 'Px8m-84T1lmLuV0gkdhGzmjz7dYBdB7mb9GyvlqE';
-export const ossBucket = 'moujitx-cloud';
+export const ossAccessKey = process.env.OSS_ACCESS_KEY;
+export const ossSecretKey = process.env.OSS_SECRET_KEY;
+export const ossBucket = process.env.OSS_BUCKET;
 export const ossFolder = __DEV__ ? 'app-myapp-dev' : 'app-myapp';
 export const ossDomain = `https://cloudfiles.moujitx.cn/${ossFolder}/`;
 
-export const pubApiAppID = 'djh0chilqtnkhxuh';
-export const pubApiAppSecret = 'CGBTEyz91iX6AArGfeDIAxGAXxR8ASxr';
+export const pubApiAppID = process.env.PUB_API_APP_ID;
+export const pubApiAppSecret = process.env.PUB_API_APP_SECRET;
 export const pubApiUrl_barcode = (key: string) =>
   `https://www.mxnzp.com/api/barcode/goods/details?barcode=${key}&app_id=${pubApiAppID}&app_secret=${pubApiAppSecret}`;
